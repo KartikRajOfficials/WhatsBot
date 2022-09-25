7b204c702f62fcc994f5865c2ec06ed84c93c456
KARTIK RAJ OFFICIAL <104128091+KartikRajOfficials@users.noreply.github.com>
2022-09-25 15:33:52 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -15,12 +15,12 @@ async function get(battery, phn_info) {
 
   return {
     msg:
-      `*Whatsbot*\n\nThis chat is Powered By *Whatsbot*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Whatsbot Version:* ${packageJson.version}\n*Pmpermit:* ${config.pmpermit_enabled}\n\n*Official Repository Url 👇*\n` +
+      `*Whatsbot*\n\nThis chat is Powered By *KINGBOT*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Whatsbot Version:* ${packageJson.version}\n*Pmpermit:* ${config.pmpermit_enabled}\n\n*Official Repository Url 👇*\n` +
       "```https://github.com/tuhinpal/WhatsBot```",
     mimetype: "image/jpeg",
     data: Buffer.from(
       (
-        await axios.get("https://telegra.ph/file/ecbc27f276890bf2f65a2.jpg", {
+        await axios.get("https://te.legra.ph/file/d8c0bf4de097ce7af4aae.jpg", {
           responseType: "arraybuffer",
         })
       ).data
@@ -48,6 +48,6 @@ module.exports = {
   command: "!start",
   commandType: "info",
   isDependent: false,
-  help: "Get information about your WhatsBot",
+  help: "Get information about your kingbot",
   execute,
 };
